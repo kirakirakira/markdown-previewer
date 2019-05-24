d017ee8eb52a1df904ad934cc770bff1879bead3
Kira Hartlage <kira.hartlage@gmail.com>
2019-05-24 17:05:49 -0400

    Added inline code to default

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ControlledInput from './ControlledInput';
+import ReactFCCtest from 'react-fcctest';
 
 function App() {
   return (
     <div>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/marked/0.6.2/marked.js" />
       <ControlledInput />
+      <ReactFCCtest />
     </div>
   );
 }
--- src/ControlledInput.js
+++ src/ControlledInput.js
@@ -11,9 +11,10 @@ class ControlledInput extends Component {
 
   componentWillMount() {
     let initialMarkdown = '# H1 Size Header \n## H2 Size Header \n' +
-    '[GitHub - kirakirakira](http://github.com/kirakirakira) \n' +
+    '[GitHub - kirakirakira](http://github.com/kirakirakira)\n' +
+    '\nI think you should use `mkdir` here instead.\n' +
     '\nkira is awesome \n' +
-    '\n```javascript \nfunction fancyAlert(arg) { \n\treturn arg + 1 \n}\n```' + '\n' +
+    '\n\`\`\`javascript \nfunction fancyAlert(arg) { \n\treturn arg + 1 \n}\n\`\`\`' + '\n' +
     '\n1. Item 1 \n' +
     '\nAs Kanye West said: \n' +
     '\n> We are living the future so \n\n> the present is our past\n' +
